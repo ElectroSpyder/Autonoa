9696657d4f8f296337b0a59800215363f46fad7b
ElectroSpyder <prg@live.com.ar>
2016-11-30 00:22:18 -0300

    Eliminacion de proyecto Servicio, creacion de Area Vehiculo

---
+++ Autonoa.Solucion.WebUI/Areas/Vehiculos/VehiculosAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Autonoa.Solucion.WebUI.Areas.Vehiculos
+{
+    public class VehiculosAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Vehiculos";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Vehiculos_default",
+                "Vehiculos/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
